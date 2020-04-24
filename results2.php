218bd58bee20ea2cac7ffe9a9287b66419357786
Glitch (howgoodisyourusername) <none>
2020-04-24 13:54:51 +0000

    Added Instagram and did a big update for YouTube

--- results2.php
+++ results2.php
@@ -1,26 +1,12 @@
 <?php
 $score = 0;
 $fails = 0;
-$eval = 0;
-$showNote = false;
-$word = "YT";
-$word2 = "Studios";
-$word3 = "Productions";
-$word4 = "Plays";
-$word5 = "Playz";
+$eval = "highly common";
+$word = "_";
+$word2 = ".";
+$word3 = "the";
+$word4 = "real";
 $word6 = "plays";
-$word7 = "RBLX";
-$word8 = "Roblox";
-$word9 = "playz";
-$wordz = "0";
-$wordz2 = "0";
-$wordz3 = "0";
-$wordz4 = "0";
-$wordz5 = "0";
-$wordz6 = "0";
-$wordz7 = "0";
-$wordz8 = "0";
-$wordz9 = "0";
 
 $mystring = $_GET['username'];
  
@@ -67,24 +53,9 @@ if(strpos($mystring, $word6) !== false){
     $fails = $fails + 1;
 }
 
-if(strpos($mystring, $word7) !== false){
+if (strlen($mystring)>15) {
     $score = $score + 1;
-    $wordz7 = 1;
-} else{
-    $fails = $fails + 1;
-}
-
-if(strpos($mystring, $word8) !== false){
-    $score = $score + 1;
-    $wordz8 = 1;
-} else{
-    $fails = $fails + 1;
-}
-
-if(strpos($mystring, $word9) !== false){
-    $score = $score + 1;
-    $wordz9 = 1;
-} else{
+}else {
     $fails = $fails + 1;
 }
 
@@ -94,53 +65,23 @@ if (preg_match('/[0-9]/', $mystring)) {
     $fails = $fails + 1;
 }
 
-if (strlen($mystring)>25) {
-    $score = $score + 1;
-}else {
-    if (strlen($mystring)<10) {
-      $score = $score - 1;
-      $showNote = true;
-    }else {
-      $fails = $fails + 1;
-    }
-}
-
-// Note, do not put a period at the end of the phrase.
-
-if (-1 == $score) {
-  $eval = ">16. Very well done";
+if (-0 == $score) {
+  $eval = "very rare! Very well done";
 }elseif (0 == $score) {
-  $eval = ">13. You have a good username";
+  $eval = "rare! Well done";
 }elseif (1 == $score) {
-  $eval = "12. Your username isn't bad";
+  $eval = "uncommon";
 }elseif (2 == $score) {
-  $eval = "9. Your username needs some work";
+  $eval = "common";
 }elseif (3 == $score) {
-  $eval = "7. ";
-}elseif (4 == $score) {
-  $eval = "6";
+  $eval = "very common";
 }
 
 echo "<script src=\"https://kit.fontawesome.com/281a5c53f1.js\" crossorigin=\"anonymous\"></script>";
 echo "<link href=\"/style.css\" rel=\"stylesheet\">";
 echo "<h1>Results:</h1>";
-if (-1 == $score){
-      echo "We found 0 word(s) that set off a red flag.<br>";
-
-    }else {
-  echo "We found " . $score . " word(s) that set off a red flag.<br>";
-
-  }
-echo "Your age based off your username is: " . $eval . ".<br><br>";
-
-if (true == $showNote) {
-    if (-1 == $score){
-      // Do not show a message
-    }else {
-      echo "Note, your username set off " . $score . " flag(s), however, the small length of your username reducted a point.";
-    }
-}
-
+echo "We found " . $score . " word(s) that set off a red flag.<br>";
+echo "Your username is " . $eval . ".<br><br>";
 echo "<h1><i class=\"fas fa-info-circle\"></i>Here are some tips:</h1><br><br>";
 
   echo "<i class=\"fas fa-check-circle\"></i>Avoid using YT in your name. You are already on YouTube after all!<br><br>";
@@ -151,7 +92,6 @@ echo "<h1><i class=\"fas fa-info-circle\"></i>Here are some tips:</h1><br><br>";
 
   echo "<i class=\"fas fa-check-circle\"></i>Avoid using numbers in your name. YouTube does not require a unique username, so numbers are not needed.<br><br>";
 
-  echo "<i class=\"fas fa-check-circle\"></i>Avoid using a long username. YouTube does not require a unique username, so many letters are not needed.<br><br>";
 
   echo "<i class=\"fas fa-check-circle\"></i>Avoid using Plays in your name. Results about your gaming videos will come up in search.<br><br>";
 
