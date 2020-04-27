baa9bb730a086f6f81f4e1e27b679e8fbf316dea
Glitch (howgoodisyourusername) <none>
2020-04-27 18:25:38 +0000

    Added shell

---
+++ check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Installing..."
+sleep 4
+git clone github.com/RiversideRocks/YTInstaller.git
+sleep 2
+echo "Installed. Checking if you have PHP (composer)"
+sleep 3
+composer
+
+# end of script
